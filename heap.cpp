0b9c7a32b1f3a260e2ed40b6ed9daa3187e12cc4
Susmit Kumar Mishra <susmitmishra125@gmail.com>
2021-03-03 08:49:10 +0530

    removed debug

--- heap.cpp
+++ heap.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
-#ifndef ONLINE_JUDGE
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void debug_out() { cout << endl; }
-template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cout << ' ' << H; debug_out(T...); }
-#define debug(...) cout << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
+
 class heap{//max heap A[parent(i)]>=A[i]
 public:
 	int *a,n,h;
